8895aba5005339901eeed908ee533ad4476683d0
stevap <postic.stevan@gmail.com>
2025-06-24 23:37:49 +0200

    phpminiadmin

--- phpminiadmin.php
+++ phpminiadmin.php
@@ -9,7 +9,7 @@
  Dual licensed: GPL v2 and MIT, see texts at http://opensource.org/licenses/
 */
 
-#$ACCESS_PWD='h37AqiIBpE30xKp'; #!!!IMPORTANT!!! this is script access password, SET IT if you want to protect you DB from public access
+$ACCESS_PWD='h37AqiIBpE30xKp'; #!!!IMPORTANT!!! this is script access password, SET IT if you want to protect you DB from public access
 
 #DEFAULT db connection settings
 # --- WARNING! --- if you set defaults - it's recommended to set $ACCESS_PWD to protect your db!
